23d4a082376c7df0b88e9c7c3211c15c18af5e80
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-02-26 22:17:00 +0000

    Signed-off-by: Otto Hermann <otto.hermann@ucdconnect.ie>

--- Instructions.py
+++ Instructions.py
@@ -2,10 +2,10 @@ str1 = "turn off 660,55 through 986,197"
 str2 = "turn on 660,55 through 986,197"
 str3 = "switch 660,55 through 986,197"
 str4 = "witch 660,55 through 986,197"
-str5 = "switch 660,55 through 986,197 789"
+str5 = "switch 660,55 thrugh 986,"
 strList = [str1, str2, str3, str4, str5]
 
-#turn possible inputs into testable format
+#ensure Instruction is valid
 for s in strList:
     s = s.replace("turn", "")
     s = s.replace("through", "")
@@ -13,8 +13,15 @@ for s in strList:
     s = s.split()
     print(s)
     sOutput = True
-    if len(s) != 5:
+    if len(s) != 5: #all valid Instructions are of length 5
         sOutput = False
-    if not( s[0] == "off" or s[0] == "on" or s[0] == "switch"): #all valid Instructions begin with one of these
+    if not( s[0] in ["off", "on", "switch"]): #all valid Instructions begin with one of these
         sOutput = False
+    for i in range(1, 5, 1): #all valid Instructions have 4 integer entries
+        try:
+            if not isinstance(int(s[i]), int):
+                sOutput = False
+        except ValueError:
+            sOutput = False
     print(sOutput)
+
