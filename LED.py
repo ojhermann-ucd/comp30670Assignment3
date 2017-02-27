0d4ab46bd01cdb9123a6a8582646956f80f3c9ce
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-02-27 23:27:57 +0000

    Signed-off-by: Otto Hermann <otto.hermann@ucdconnect.ie>

---
+++ LED.py
@@ -0,0 +1,17 @@
+#imports
+import sys
+import Grid
+import Instructions
+import Modification
+import Links
+import urllib.request
+from urllib.error import URLError, HTTPError
+
+#create sourceList
+sourceList = Links.createLinkList("LinksSource.txt")
+
+#create gridSizeList
+gridSizeList = Links.createGridSizeList(sourceList)
+
+print(sourceList)
+print(gridSizeList)
