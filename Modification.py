cb016d89b59d53baef70e3ec22867b42333c3230
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-02-27 13:30:59 +0000

    Signed-off-by: Otto Hermann <otto.hermann@ucdconnect.ie>

--- Modification.py
+++ Modification.py
@@ -2,9 +2,8 @@ testGrid = {(0, 0): False, (0, 1): False, (0, 2): False, (1, 0): False, (1, 1): 
 
 testInstruction = ["switch", (1,1), (2, 2)]
 
-
-def modificationCommand(theList):
-    return theList[0]
+def modificationCommand(theList): #list input
+    return theList[0] #str output
 
 #generate the startTuple
 def modificationTupleStart(theList): #list input
@@ -13,3 +12,30 @@ def modificationTupleStart(theList): #list input
 #generate the endtTuple
 def modificationTupleEnd(theList): #list input
     return theList[2] #tuple output
+
+def modificationModificationMicro(theCommand, theBool):
+    if theCommand == "on":
+        return True
+    if theCommand == "off":
+        return False
+    if theCommand == "switch":
+        if theBool == True:
+            return False
+        else:
+            return True
+
+def modificationModification(theList, theGrid): #input list and dictionary
+    #variables
+    theCommand = theList[0]
+    xStart = theList[1][0]
+    yStart = theList[1][1]
+    xEnd = theList[2][0]
+    yEnd = theList[2][1]
+    
+    #modification
+    for x in range(xStart, xEnd + 1, 1):
+        for y in range(yStart, yEnd + 1, 1):
+            theTuple = (x, y)
+            theBool = theGrid.get(theTuple,)
+            theGrid[theTuple] = modificationModificationMicro(theCommand, theBool)
+    return theGrid #output dictionary
