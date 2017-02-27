271425a555da027dae7c5a6dcddd20636a157760
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-02-27 12:42:06 +0000

    Signed-off-by: Otto Hermann <otto.hermann@ucdconnect.ie>

--- Grid.py
+++ Grid.py
@@ -1,23 +1,27 @@
 #turns input into a list after removing leading whitespace
-def gridInputRemoveLeftWhiteSpace(str):
+def gridInputRemoveLeftWhiteSpace(str): #string input
     str = str.lstrip()
     str = str.split() 
     return str #returns a list
 
 #checks if length is one i.e. only one number was given
-def gridCheckLength(theList):
-    return (len(theList) == 1)
+def gridCheckLength(theList): #list input
+    return (len(theList) == 1) #boolean output
 
 #checks if the entry is a number
-def gridCheckInt(theList):
-    return isinstance(int(theList[0]), int)
+def gridCheckInt(theList): #list input
+    return isinstance(int(theList[0]), int) #boolean output
 
 #generates the integer
-def gridMakeInt(theList):
-    return int(theList[0])
+def gridMakeInt(theList): #list input
+    return int(theList[0]) #int output
 
-#generates the grid
-def gridMakeGrid(theInt):
+#makes sure theInt is positive
+def gridCheckIntSign(theInt): #int input
+    return (theInt > -1) #boolean output
+
+#generates the grid as a dictionary of (x, y) : boolean key : value pairs
+def gridMakeGrid(theInt): #int input
     row = 0
     column = 0
     theGrid = {}
@@ -27,4 +31,4 @@ def gridMakeGrid(theInt):
             column += 1
         column = 0
         row += 1
-    return theGrid
+    return theGrid #dict output
