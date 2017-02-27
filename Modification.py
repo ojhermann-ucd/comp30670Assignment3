b5019e769d7aedeb3ad18d0a50d44531190756c2
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-02-27 18:12:17 +0000

    Signed-off-by: Otto Hermann <otto.hermann@ucdconnect.ie>

--- Modification.py
+++ Modification.py
@@ -1,10 +1,6 @@
 from Grid import *
 from Instructions import *
 
-testGrid = {(0, 0): False, (0, 1): False, (0, 2): False, (1, 0): False, (1, 1): False, (1, 2): False, (2, 0): False, (2, 1): False, (2, 2): False}
-
-testInstruction = ["switch", 1, 1, 2, 2]
-
 def modificationEntry(theI, theBool): #Instruction and boolean inputs
     if theI.command == "on":
         return True
