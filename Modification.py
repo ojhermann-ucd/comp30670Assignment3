11a3a51af74a3696117038df26b1c073f3770663
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-02-27 12:55:20 +0000

    Signed-off-by: Otto Hermann <otto.hermann@ucdconnect.ie>

--- Modification.py
+++ Modification.py
@@ -3,6 +3,9 @@ testGrid = {(0, 0): False, (0, 1): False, (0, 2): False, (1, 0): False, (1, 1): 
 testInstruction = ["switch", (1,1), (2, 2)]
 
 
+def modificationCommand(theList):
+    return theList[0]
+
 #generate the startTuple
 def modificationTupleStart(theList): #list input
     return theList[1] #tuple output
