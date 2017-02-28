3c87196febd006238eb73b527dcceabdf56b0097
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-02-28 22:23:17 +0000

    Signed-off-by: Otto Hermann <otto.hermann@ucdconnect.ie>

--- setup.py
+++ setup.py
@@ -5,5 +5,8 @@ from os import name
 setup(
     name = "comp30670Assignment3 by Otto Hermann aka 16203034",
     description = "comp30670 Assignment 3, Spring 2017",
-    
+    author = "Otto Hermann",
+    author_email = "otto.hermann@ucdconnect.ie",
+    url="www.haha-no-i-do-not-have-one.org",
+    packages = ['comp30670Assignment3']
     )
