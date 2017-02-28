37f71df04b2132fca4713fd0bbaee8dffb2be681
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-02-28 12:31:13 +0000

    Signed-off-by: Otto Hermann <otto.hermann@ucdconnect.ie>

--- LED.py
+++ LED.py
@@ -18,6 +18,29 @@ gridSizeList = Grid.gridCreateSizeList(sourceList)
 #create grids
 testGrid = Grid.Grid(gridSizeList[0]).grid
 
+def sumLED(gridSize, theSource): #int, str
+    theGrid = Grid.Grid(gridSize).grid
+    theSource = urllib.request.urlopen(theSource)
+    for line in theSource:
+        theLine = str(line, 'utf-8')
+        #formatting
+        theLine = Instructions.instructionFormat(theLine)
+        #test
+        if not Instructions.instructionValidTypes(theLine):
+            pass
+        else:
+            theLine = Instructions.instructionValidRange(theLine, gridSize)
+            if not Instructions.instructionValidOrder(theLine):
+                pass
+            else:
+                theLine = Instructions.Instruction(theLine)
+                theGrid = Modification.modificationGrid(theLine, theGrid)
+                gridSum = sum(theGrid.values())
+    return gridSum
+
+print(sumLED(int(gridSizeList[0]), sourceList[0]))
+
+
 #execute instructions
 theUpperBound = int(gridSizeList[0])
 theSource = sourceList[0]
